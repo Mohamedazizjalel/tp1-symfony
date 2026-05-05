bbbe4a3744ac57257bb20717de239e664a85cbd5
Mohamedazizjalel <mohamedazizjalel@gmail.com>
2026-05-05 22:54:25 +0100

    Update AccueilController.php

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -19,6 +19,6 @@ class AccueilController extends AbstractController
     #[Route('/bonjour/{prenom}', name: 'app_bonjour')]
     public function bonjour(string $prenom): Response 
     {
-        return new Response("<h1>Bonjour $prenom ! Bienvenue sur Symfony 7.4</h1>");
+        return new Response("<h1>Bonjour $prenom !</h1>");
     }
 }
